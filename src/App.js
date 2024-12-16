1a2c5879a2082c9c6c3e1c8bbba2fd9cfa3f6e0f
zmmxxi <milazay24@yandex.ru>
2024-12-16 22:20:57 +0300

    add FIO

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const greeting = "Это тестовый проект Зайцевой М.М.";
 
   console.log('This is App ');
   return (
