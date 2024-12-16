46c8a4abf13bec7d39d4a866d9b1359033bb0019
zmmxxi <130219971+zmmxxi@users.noreply.github.com>
2024-12-16 22:36:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const greeting = "Это тестовый проект Зайцевой М.М. - commit chore";
 
-  console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
